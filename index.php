c5749d95079bd8da3c7e5edb4719ea21fbcd82f5
JETTLIFE TECH <daine@jettlifetech.com>
2024-02-07 06:49:02 +0000

    content update to skill sectiontion

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 <body>
 <?php include '/var/www/dainedvorak.com/z_dev/public/php-dynamic-content/navbar.html';?>
 
+<hr>
+<?php include '/var/www/dainedvorak.com/z_dev/public/php-dynamic-content/facts.html';?>
 
 
     <!-- Jumbotron -->
@@ -20,44 +22,18 @@
  <div class="container">
     <div class="row">
         <div class="col-2">
-        <div class="blockcode">
-  <div class="header">Footer</div>
-  <div class="description">Footer</div>
-  <footer class="shadow">
-    <div
-      class="d-flex justify-content-between align-items-center mx-auto py-4 flex-wrap"
-      style="width: 80%"
-    >
-      <a href="/#" class="d-flex align-items-center p-0 text-dark">
-        <img alt="logo" src="../img/logo.png" width="30px" />
-        <span class="ms-4 h5 font-weight-bold">Devwares</span>
-      </a>
-      <small>&copy; Devwares, 2020. All rights reserved.</small>
-      <div>
-        <button class="btn btn-dark btn-flat p-2">
-          <i class="fa fa-facebook"></i>
-        </button>
-        <button class="btn btn-dark btn-flat p-2">
-          <i class="fa fa-twitter"></i>
-        </button>
-        <button class="btn btn-dark btn-flat p-2">
-          <i class="fa fa-instagram"></i>
-        </button>
-      </div>
-    </div>
-  </footer>
-</div>
+        
 
-        </div>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
         <div class="col-8">
             <!-- Card with Form -->
-            <div class="card">
-                <div class="card-body">
-                    <div class=container-flex bg-dark>
-                        
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <div class="col-2"></div>
     </div>
