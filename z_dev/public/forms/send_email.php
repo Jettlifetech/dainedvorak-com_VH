3ae9f389f5e2776d745bc05f8b8b168ac9fdc0e9
JETTLIFE TECH <daine@jettlifetech.com>
2024-02-07 20:15:17 +0000

    updating site content - finalized skill list design

---
+++ z_dev/public/forms/send_email.php
@@ -0,0 +1,40 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use vendor/phpmailer/phpmailer/src/SMTP.php;
+
+
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'dainedvorak@gmail.com';                // SMTP username
+    $mail->Password   = 'your_password_here';                   // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('dainedvorak@gmail.com', 'Mailer');
+    $mail->addAddress('recipient@example.com', 'Joe User');     // Add a recipient, Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+
+    // Content
+    $mail->isHTML(true);                                        // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
