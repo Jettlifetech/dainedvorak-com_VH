4199818f13bb01ba8bbf744392a3ca669487b708
JETTLIFE TECH <daine@jettlifetech.com>
2024-02-08 03:34:46 +0000

    update-site-dev

--- z_dev/public/forms/send_email.php
+++ z_dev/public/forms/send_email.php
@@ -1,6 +1,6 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use vendor/phpmailer/phpmailer/src/SMTP.php;
+//use vendor/phpmailer/phpmailer/src/SMTP.php;
 
 
 use PHPMailer\PHPMailer\Exception;
@@ -17,7 +17,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'dainedvorak@gmail.com';                // SMTP username
-    $mail->Password   = 'your_password_here';                   // SMTP password
+    $mail->Password   = 'osmr bhmj xmlt dipj';                   // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -37,4 +37,3 @@ try {
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
