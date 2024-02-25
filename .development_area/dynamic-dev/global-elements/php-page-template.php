77e750c9a8f869e64be38dc5583951c3c794b37a
JETTLIFE TECH <daine@jettlifetech.com>
2024-02-25 21:45:08 +0000

    pre-dynamic navbar development and testing

---
+++ .development_area/dynamic-dev/global-elements/php-page-template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Web Page</title>
+    <?php include 'header.html'; ?>
+</head>
+<body>
+
+
+<?php include 'navbar.html'; ?>
+<?php include 'body-1.html'; ?>
+<!-- Main content of your webpage -->
+<div>
+    <h1>Welcome to My Website 2</h1>
+    <p>This is the main content of the homepage.</p>
+</div>
+
+<?php include 'footer.html'; ?>
+
+</body>
+</html>
---
+++ .development_area/dynamic-dev/v2-dainedvorak-com_TO-DO-List.md
@@ -0,0 +1,7 @@
+- [ ] #0 Push update to Github
+- [ ] #1 - create dynamic nav #1
+- [ ] #2- update all pages in domain folder
+- [ ] #3 Push update to Github
+- [ ] setup dynamic header
+
+
