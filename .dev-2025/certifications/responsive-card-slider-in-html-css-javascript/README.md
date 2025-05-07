99ad928a87dbaf8c8dfe707e0b651215f72744d2
Jettlifetech <daine@jettlifetech.com>
2025-05-07 17:19:54 +0000

    All repo update with final new cert slider

---
+++ .dev-2025/certifications/responsive-card-slider-in-html-css-javascript/README.md
@@ -0,0 +1,6 @@
+# Responsive Card Slider in HTML CSS & JavaScript
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/mal-chut/pen/oNmvwbR](https://codepen.io/mal-chut/pen/oNmvwbR).
+
---
+++ includes/navigation.php
@@ -0,0 +1,21 @@
+<nav class="main-nav">
+    <div class="container">
+        <div class="logo">
+            <a href="/">Daine Dvorak</a>
+        </div>
+        
+        <button class="mobile-menu-toggle" aria-label="Toggle navigation menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        
+        <ul class="nav-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/services.php">Services</a></li>
+            <li><a href="/portfolio.php">Portfolio</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
