8cba1731ebea0a2b51ebc9f7dc89ec1a0054aee2
Jettlifetech <daine@jettlifetech.com>
2025-05-16 14:33:01 +0000

    getting ready for prod launch; fixing odds and ends, adding recommendations

--- testimonial-slider/script.js
+++ testimonial-slider/script.js
@@ -6,7 +6,7 @@ const personName = document.getElementById("personName");
 const profession = document.getElementById("profession");
 const description = document.getElementById("description");
 const surpriseMeBtn = document.getElementById("surpriseMeBtn");
-const chicken = document.querySelector(".chicken");
+//const chicken = document.querySelector(".chicken");
 
 let isChickenVisible;
 
@@ -152,24 +152,6 @@ function setNextCardRight() {
 
 leftArrow.addEventListener("click", setNextCardLeft);
 rightArrow.addEventListener("click", setNextCardRight);
-
-surpriseMeBtn.addEventListener("click", () => {
-	if (chicken.style.opacity === "0") {
-		chicken.style.opacity = "1";
-		imgDiv.classList.add("move-head");
-		surpriseMeBtn.innerText = "Remove the chicken";
-		surpriseMeBtn.classList.remove("surprise-me-btn");
-		surpriseMeBtn.classList.add("hide-chicken-btn");
-		isChickenVisible = true;
-	} else if (chicken.style.opacity === "1") {
-		chicken.style.opacity = "0";
-		imgDiv.classList.remove("move-head");
-		surpriseMeBtn.innerText = "Surprise me";
-		surpriseMeBtn.classList.add("surprise-me-btn");
-		surpriseMeBtn.classList.remove("hide-chicken-btn");
-		isChickenVisible = false;
-	}
-});
 
 window.addEventListener("resize", () => {
 	description.style.height = "100%";
